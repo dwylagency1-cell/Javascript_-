6f37bd2f9d206fff8d89511da4a4f66f594ad2bd
Dekiru <dwyl.agency1@gmail.com>
2026-03-25 21:43:27 +0530

    todo_list.js

--- todo_list.js
+++ todo_list.js
@@ -1,7 +1,39 @@
-const todoList = []
+const todoList = [{
+    name : 'make dinner',
+    date: '2026-12-22'
+    
+}, {
+    name: 'wash dishes',
+    date: '2026-12-22'
+} ]
+
+render_todoList();
+function render_todoList() {
+    let todoHTML = '';
+    for (let i = 0; i < todoList.length; i++) {
+        
+        const name = todoList[i].name
+        const date = todoList[i].date
+        const html = `<p>                            
+        ${name} ${date}
+        <button onclick= "
+        todoList.splice(${i}, 1);
+        render_todoList()
+        ">Delete</button></p>`
+        todoHTML = todoHTML + html
+    }
+    console.log(todoHTML)
+    document.querySelector('.js_div').innerHTML = todoHTML;
+}
 
 function add_input() {
-    todoList.push(document.querySelector('.add_todos').value)
+    todoList.push({
+        name: `${document.querySelector('.add_todos').value}`,
+        date: `${document.querySelector('.date_time').value}`
+    });
     console.log(todoList)
     document.querySelector('.add_todos').value = ""
+
+    render_todoList();
+
 }
