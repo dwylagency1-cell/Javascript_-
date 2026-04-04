fee7252ee46939261f4c3be145d7270bfa10263d
Dekiru <dwyl.agency1@gmail.com>
2026-04-04 23:25:49 +0530

    Mini_Shop_data.js

---
+++ Mini_Shop_data.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    name: "iPhone 14",
+    price: 79999,
+    orders: 1200
+  },
+  {
+    name: "Dell Laptop",
+    price: 65000,
+    orders: 850
+  },
+  {
+    name: "Boat Headphones",
+    price: 1999,
+    orders: 3200
+  },
+  {
+    name: "Samsung Monitor",
+    price: 12000,
+    orders: 540
+  },
+  {
+    name: "Gaming Mouse",
+    price: 999,
+    orders: 2100
+  }
+];
+
