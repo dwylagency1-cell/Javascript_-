dcd3a9ea25d529974836cc001a35fbaf9e7f887b
Dekiru <dwyl.agency1@gmail.com>
2026-04-08 14:33:01 +0530

    foods.js

---
+++ food_delivery_app/foods.js
@@ -0,0 +1,142 @@
+const foods = [
+  {
+    id: "f1",
+    name: "Cheese Burger king size | Non Veg",
+    price: 1200,
+    rating: 4.5,
+    image: "image/burger.jpg"
+  },
+  {
+    id: "f2",
+    name: "Garden Fresh Vegetable Pizza",
+    price: 2500,
+    rating: 4.2,
+    image: "image/pizza.jpg"
+  },
+  {
+    id: "f3",
+    name: "Hand-Cut Russet Potato Fries",
+    price: 900,
+    rating: 4.0,
+    image: "image/fries.jpg"
+  },
+  {
+    id: "f4",
+    name: "Velvety Classic Creamy Cold Coffee",
+    price: 1500,
+    rating: 4.6,
+    image: "image/cold-coffee.jpg"
+  },
+  {
+    id: "f5",
+    name: "Smoky Tandoori Paneer Tikka Kathi Roll",
+    price: 1300,
+    rating: 4.3,
+    image: "image/roll.jpg"
+  },
+  {
+    id: "f6",
+    name: "Slow-Cooked Chicken Dum Biryani",
+    price: 3000,
+    rating: 4.7,
+    image: "image/biryani.jpg"
+  },
+  {
+    id: "f7",
+    name: "Velvety Chocolate Cream Shake",
+    price: 1800,
+    rating: 4.4,
+    image: "image/shake.jpg"
+  },
+  {
+    id: "f8",
+    name: "Authentic South Indian Crispy Golden Masala Dosa",
+    price: 1100,
+    rating: 4.5,
+    image: "image/dosa.jpg"
+  },
+  {
+    id: "f9",
+    name: "Creamy Fettuccine Alfredo with Parmesan",
+    price: 2200,
+    rating: 4.1,
+    image: "image/pasta.jpg"
+  },
+  {
+    id: "f10",
+    name: "The Ultimate Decadent Hot Fudge Sundae",
+    price: 1400,
+    rating: 4.6,
+    image: "image/icecream.jpg"
+  },
+  {
+    id: "f11",
+    name: "Crispy Oven-Baked Chicken Wings",
+    price: 2600,
+    rating: 4.5,
+    image: "image/wings.jpg"
+  },
+  {
+    id: "f12",
+    name: "Steamed Vegetable Dumplings",
+    price: 1000,
+    rating: 4.3,
+    image: "image/momos.jpg"
+  },
+  {
+    id: "f13",
+    name: "Authentic Punjabi Chana Bhatura ",
+    price: 1200,
+    rating: 4.4,
+    image: "image/chole.jpg"
+  },
+  {
+    id: "f14",
+    name: "Crispy Golden Grilled Cheese Sandwich",
+    price: 900,
+    rating: 4.2,
+    image: "image/sandwich.jpg"
+  },
+  {
+    id: "f15",
+    name: "Authentic Charcoal-Grilled Tandoori Chicken",
+    price: 3200,
+    rating: 4.7,
+    image: "image/tandoori.jpg"
+  },
+  {
+    id: "f16",
+    name: "Crispy Golden Vegetable Spring Rolls",
+    price: 1100,
+    rating: 4.1,
+    image: "image/spring-roll.jpg"
+  },
+  {
+    id: "f17",
+    name: "Layered Falooda Cream Dream",
+    price: 1600,
+    rating: 4.5,
+    image: "image/falooda.jpg"
+  },
+  {
+    id: "f18",
+    name: "Maggi 2-Minute Instant Masala Noodles",
+    price: 700,
+    rating: 4.0,
+    image: "image/maggi.jpg"
+  },
+  {
+    id: "f19",
+    name: "Mangalorean Style Spicy Fish Curry",
+    price: 2800,
+    rating: 4.6,
+    image: "image/fish-curry.jpg"
+  },
+  {
+    id: "f20",
+    name: "Sweet Brownie",
+    price: 1300,
+    rating: 4.5,
+    image: "image/brownie.jpg"
+  }
+];
