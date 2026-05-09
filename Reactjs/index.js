32ff753fc72ad19fd3d02e35487eda66dcf8ab2c
Dekiru <dwyl.agency1@gmail.com>
2026-05-09 22:00:45 +0530

    index.js

---
+++ Reactjs/index.js
@@ -0,0 +1,10 @@
+const h1 = document.createElement('h1')
+
+h1.innerHTML = 'Hello from js'
+document.body.appendChild(h1)
+
+import arr from './app.js'
+console.log(arr)
+
+import { num } from './app.js'
+console.log(num)
